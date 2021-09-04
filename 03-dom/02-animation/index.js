5951cf2e706cebbc3243c7f23923249424aa56f1
miladpeagh <miladpeagh1999@gmail.com>
2021-09-04 23:16:06 +0430

    make text animation

--- 03-dom/02-animation/index.js
+++ 03-dom/02-animation/index.js
@@ -7,7 +7,7 @@ tag.style.width='50%';
 function startAnimatio(){
 	let elem = document.getElementById('animation');
 	let pos = 0;
-	let id = setInterval(frame,10);
+	let id = setInterval(frame,50);
 
 	function frame(){
 		if(pos == 35){
@@ -19,3 +19,27 @@ function startAnimatio(){
 		}
 	}
 }
+
+(function write(){
+	let el =document.getElementById('demo');
+	let text = -1;
+	let id = setInterval(frame,50);
+	let varaible = 'Lorem ipsum dolor sit amet';
+	function frame(){
+		let array1 = varaible.split('');
+		if (text == array1.length - 1) {
+			clearInterval();
+		}else{
+			text++;
+			el.innerHTML += array1[text]; 
+		}
+
+	}
+	// let test = frame();
+	// function test1(){
+	// 	for (let i = 0; i < test.length; i++){
+	// 		el.innerHTML += test[i];
+	// 	}
+	// }
+	// setInterval(test1(),100);
+})();
